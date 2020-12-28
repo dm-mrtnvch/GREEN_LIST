a8819b650239054ea13131374be6e67eab3cc11f
DM-MRTNVCH <dmvshn@gmail.com>
2020-12-28 23:03:36 +0300

    Destructuring

---
+++ src/06-destructuring-assignment/Destructuring.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {LessonType, ManTypeStreet} from './destructuring.test';
+
+export type ManType = {
+    name: string
+    age: number
+    lessons: Array<LessonType>
+    address: ManTypeStreet
+}
+
+type PropsType = {
+    title: string
+    man: ManType
+    food: Array<string>
+    car: {model: string}
+}
+
+
+function useDimychState(m: string) {
+    return [m, function(){}]
+}
+
+function useDimychState2(m: string) {
+    return {
+        message: m,
+        setMessage: function(){}
+    }
+}
+
+
+export const ManComponent: React.FC<PropsType> = ({title, man, ...restProps}) => {
+
+    // const [message, setMessage] = useDimychState('hello')
+    const [message, setMessage] = useDimychState2('hello')
+
+    return <div>
+        <h1>{title}</h1>
+        <hr/>
+        <div>
+            {man.name}
+            {restProps.car.model}
+            {restProps.food}
+        </div>
+    </div>
+}
