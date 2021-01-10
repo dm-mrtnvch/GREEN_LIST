4ddac073a7c4464f422a8d0688b06bd6abdb7f69
DM-MRTNVCH <dmvshn@gmail.com>
2021-01-10 23:35:28 +0300

    immutability (part 2)

--- src/10/10_01.test.ts
+++ src/10/10_01.test.ts
@@ -1,21 +1,41 @@
-import {UserType} from './10_01';
-import makeHairStyle from './10_01'
-
+import {UserType, UserWithLaptopType, makeHairStyle, moveUser} from './10_01';
 
 test('change address', () => {
     let user: UserType = {
         name: 'Dimych',
         hair: 32,
         address: {
-            title: 'Minsk'
+            city: 'Minsk',
+            house: 14
         }
     }
 
     const awesomeUser = moveUser(user, 2)
 
-    user = awesomeUser
-
     expect(awesomeUser.hair).toBe(16)
     expect(user.hair).toBe(32)
     expect(awesomeUser.address).toBe(user.address)
+})
+
+
+test('for change address', () => {
+    let user: UserWithLaptopType = {
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    }
+
+    const awesomeUser = moveUser(user, 'Kiev')
+
+    expect(user).not.toBe(movedUser)
+    expect(user.address).not.toBe(movedUser.address)
+    expect(user.laptop).toBe(movedUser.laptop)
+    expect(movedUser.address.title).toBe('Kiev')
+
 })
